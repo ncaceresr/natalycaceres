5cf453c8279e7913995a793f23013f86b6355c7d
ncaceresr <caceres46@gmail.com>
2022-05-16 23:09:12 -0400

    actualizacion

--- admin/contenido.php
+++ admin/contenido.php
@@ -45,17 +45,19 @@ require_once '../clases/contenido.php';
 
 $contenido = new Contenidos();
 /*$contenido->autor_idusuario=1;
-$contenido->idclasificaciones=2;
+$contenido->idclasificaciones=3;
 $contenido->imagen= "";
-$contenido->titulo= '¡Hola a todos!';
-$contenido->subtitulo= 'pruebas2';
+$contenido->titulo= '¡Hola!';
+$contenido->subtitulo= 'pruebas';
 $contenido->contenido= 'lorem ipsum dolor sit amet, consectet';
 
 $contenido->agregar();*/
 
 $contenido->idcontenido='2';
 $contenido->obtener();
+
 ?>
+
 
 <?php   
 require_once '../clases/clasificaciones.php';
@@ -65,22 +67,23 @@ require_once '../clases/clasificaciones.php';
     
     
     ?>
-ID:  <?php echo $clasificaciones->idclasificaciones; ?> <br>
-Nombre: <?php echo $clasificaciones->nombre; ?> <br>
+<!--ID:  <?php echo $clasificaciones->idclasificaciones; ?> <br>
+Nombre: <?php echo $clasificaciones->nombre; ?> <br> -->
 
-<div class=container>
+<div class="container">
     <div class="card  w-75 align-item: center">
         <div class="row margin-left: 20px">
-           <h2>Contenido</h2>
+            <h2>Contenido</h2>
         </div>
         <div class="card-body">
             <div class="row">
                 <div class="col-3">
-                    <label  for="clasificacion" class=label-primary name="clasificacion">Clasificación</label>
+                    <label  for="clasificacion" class="form-label" name="clasificacion">Clasificación</label>
+                    
                 </div>
                 <div class="col-9">
-                    <select class="form-control"> 
-                        <option selected>Elija una opción</option>
+                    <select class="form-select" > 
+                        <option selected >Elija una opción</option>
                         <option value=" <?php   
                         require_once '../clases/clasificaciones.php';
                         $clasificaciones=new Clasif();
@@ -105,12 +108,49 @@ Nombre: <?php echo $clasificaciones->nombre; ?> <br>
                         $clasificaciones->idclasificaciones=4;
                         $clasificaciones->listar(); ?>" >  <?php echo $clasificaciones->idclasificaciones;?> . <?php echo $clasificaciones->nombre; ?>               
                         </option>
+                    </select>
                 </div>
-            </div>
+            </div> <br>
+
+            <div class="row">
+                <div class="col-3">
+                <label for="imagen"class="form-label" name="imagen">Imagen</label>
+                </div>
+                <div class="col-9">
+                <input type="url" class="form-control" name="url" placeholder="URL de imágen">
+                </div>
+            </div> <br>
+
+            <div class="row">
+                <div class="col-3">
+                <label for="titulo"class="form-label" name="titulo" >Título</label>
+                </div>
+                <div class="col-9">
+                <input type="text" class="form-control" name="titulo" placeholder="Título del Contenido">
+                </div>
+            </div><br>
+
+            <div class="row">
+                <div class="col-3">
+                <label for="resumen"class="form-label" name="titulo">Resumen</label>
+                </div>
+                <div class="col-9">
+                <textarea  type="text" class="form-control" name="Resumen" placeholder="Resumen del Contenido"></textarea>
+                </div>
+            </div><br>
+
+            <div class="row">
+                <div class="col-3">
+                <label for="contenido"class="form-label" name="contenido">Contenido</label>
+                </div>
+                <div class="col-9">
+                <textarea  type="text" class="form-control" name="contenido" placeholder="Texto del contenido"></textarea>
+                </div>
+            </div><br>
+
         </div>
-        <div class="card-footer text-muted">
-            2 days ago
         </div>
+
     </div>
 </div>
 
